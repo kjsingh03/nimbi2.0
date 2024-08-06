430e801c9247a2c029ea5266e37e371080336b0d
Karanjot SIngh <k.jsingh.1200@gmail.com>
2024-08-07 04:30:30 +0530

    feat

--- src/components/Card/IntroCard.jsx
+++ src/components/Card/IntroCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function IntroCard({ img, title, desc }) {
     return (
-        <div className="flex flex-col items-center justify-center xl:justify-center gap-[16px] w-full md:w-[34%] xl:w-[26.85%] xs:max-w-[502px] 2xl:w-[502px] md:border-l border md:border-b-0 border-[#767f84] h-[18rem] xs:h-[17rem] sm:h-[15rem] md:h-[26rem] lg:h-[24rem] 2xl:h-[21rem] py-[42px] px-[13px] ">
+        <div className="flex flex-col items-center justify-center xl:justify-center gap-[16px] w-full md:w-[34%] xl:w-[26.85%] xs:max-w-[502px] 2xl:w-[502px] md:border-l border md:border-b-0 border-[#767f84] h-[339px] py-[42px] px-[13px] ">
             <div className="w-24">
                 <img src={img} alt="" className='w-full h-full object-fill' />
             </div>
